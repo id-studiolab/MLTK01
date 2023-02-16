7c95d5d1412e8bfb75bd76a1f21eefa6c80b5509
Lenny Martinez <lennymartinezd@gmail.com>
2023-02-16 11:34:48 +0100

    Try to regenerate API

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,18 @@
-var gulp = require( 'gulp' );
+var gulp = require('gulp');
 
-const fs = require( 'fs' )
-const jsdoc2md = require( 'jsdoc-to-markdown' )
+const fs = require('fs');
+const jsdoc2md = require('jsdoc-to-markdown');
 
-gulp.task( 'generate-doc', ( done ) => {
-
+gulp.task('generate-doc', done => {
   var jsdocOptions = {
     files: 'library/*.js', // specify where your files are
-    template: fs.readFileSync( 'docs/api-template.hbs', 'utf8' ), // read a template file
+    template: fs.readFileSync('./docs/api-template.hbs', 'utf8'), // read a template file
     //template: "ciao",
     //template: '---\nlayout: default\ntitle: api\nnav_order: 5\n---\n\n#MLTK API\n\n***\n{{>main}}',
     'example-lang': 'js', // specify the "@example" code block language
     noCache: true, // Bypass caching
-  }
+  };
 
-  const output = jsdoc2md.render( jsdocOptions ).then(
-    output => fs.writeFileSync( 'docs/api.md', output )
-  );
+  const output = jsdoc2md.render(jsdocOptions).then(output => fs.writeFileSync('docs/api.md', output));
   return done();
-} )
+});
